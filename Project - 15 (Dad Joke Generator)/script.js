d74f51fb831ca72b0743da1da6c04b531d59925b
Aditya <adityadevansh2002@gmail.com>
2024-08-17 14:26:00 +0530

    added Dad Joke Generator

---
+++ Project - 15 (Dad Joke Generator)/script.js
@@ -0,0 +1,34 @@
+let btn = document.querySelector('button');
+let jokeDiv = document.querySelector('.jokes');
+async function fetchData(){
+    try{
+    jokeDiv.textContent = "Dady is making a joke for you, Please wait..."
+    let response = await fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            'Accept': 'text/plain'
+        }
+    });
+    let data = await response.text();
+    // console.log(data);
+    updateDataOnUI(data);
+    }
+    catch(error){
+        jokeDiv.innerHTML = '';
+        jokeDiv.innerHTML = `<h1>An Error Occured, try again later!!</h1>`
+    }
+}
+
+btn.addEventListener('click', (e)=>{
+    fetchData();
+    e.target.textContent = "Loding Dad's Joke!!";
+    setTimeout(() => {
+        e.target.textContent = "Click Me!!"
+    }, 2200);
+});
+
+function updateDataOnUI(joke){
+    // jokeDiv.innerHTML = '';
+    setTimeout(()=>{
+    jokeDiv.innerHTML = `<h2>${joke}</h2>`;    
+    }, 1800);
+}
